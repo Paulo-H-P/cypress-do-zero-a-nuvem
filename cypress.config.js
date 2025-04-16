9d3856f9ce4d6e972dfc3a73eef86a34ab71a2a7
Paulo-H-P <fredysoh@gmail.com>
2025-04-16 16:32:07 -0300

    Configure cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
-  projectId: 'a99xxi'
+  projectId: 'a99xxi', // 👈 vírgula aqui!
   viewportHeight: 880,
   viewportWidth: 1280,
   e2e: {},
